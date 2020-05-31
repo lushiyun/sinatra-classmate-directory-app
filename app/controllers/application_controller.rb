ccccd0c19e517541d1c9667f913d6d13adbf2e86
github username <github email address>
2020-05-31 19:05:59 -0400

    Add ApplicationController helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,29 @@ class ApplicationController < Sinatra::Base
   configure do
     enable :sessions
     set :session_secret, "top_secret"
-
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+  
+    def current_user
+      @user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  
+    def authentication_required
+      if !logged_in?
+        flash[:notice] = "You must log in."
+        redirect to "/"
+      end
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_31_220334) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
