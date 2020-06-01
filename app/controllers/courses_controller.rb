7bee928d43418bbb6fca49874518b8cb9d6d89b4
github username <github email address>
2020-06-01 00:55:06 -0400

    Add user log out and course index

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,3 +1,13 @@
 class CoursesController < ApplicationController
+  before '/courses/*' do
+    authentication_required
+  end
+
+  get '/courses' do
+    @courses = current_user.courses
+    erb :"/courses/index"
+  end
+
+  
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect to "/courses/index"
+      redirect to "/courses"
     else 
       flash[:alerts] = ["Wrong email or password. Please try again."]
       redirect to "/login"
@@ -24,10 +24,15 @@ class UsersController < ApplicationController
     if user.valid?
       user.save
       session[:user_id] = user.id
-      redirect to "/courses/index"
+      redirect to "/courses"
     else 
       flash[:alerts] =  user.errors.full_messages
       redirect to "/signup"
     end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to "/"
   end
 end
