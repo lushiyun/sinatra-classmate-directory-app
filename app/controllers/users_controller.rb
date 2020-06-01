b912d1c089098de376e8be827997feac42f5a5a5
github username <github email address>
2020-06-01 00:37:36 -0400

    Add user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,20 +9,25 @@ class UsersController < ApplicationController
   end
 
   post '/login' do 
-    user = User.find_by
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/courses/index"
+    else 
+      flash[:alerts] = ["Wrong email or password. Please try again."]
+      redirect to "/login"
+    end
   end
 
   post '/signup' do
-    @user = User.new(params)
-    if @user.valid?
-      @user.save
-      session[:user_id] = @user.id
+    user = User.new(params)
+    if user.valid?
+      user.save
+      session[:user_id] = user.id
       redirect to "/courses/index"
     else 
-      flash[:alerts] =  @user.errors.full_messages
+      flash[:alerts] =  user.errors.full_messages
       redirect to "/signup"
     end
-
   end
-
 end
