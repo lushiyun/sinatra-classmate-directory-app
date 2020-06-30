b0796cbefbb49496aa95a2a6afe0a5cbe09a0e99
github username <lushiyun@outlook.com>
2020-06-30 14:54:40 -0400

    Change Sinatra config public for deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     enable :sessions
     set :session_secret, "#{ENV['SESSION_SECRET']}"
-    set :public_folder, Proc.new { File.join(root, "public") }
+    set :public_folder, 'public'
     set :views, 'app/views'
   end
 
